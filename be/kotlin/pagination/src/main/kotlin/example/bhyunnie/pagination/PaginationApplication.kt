f48e7db293ea3c1365e5c95008d6aa7104ff9476
bhyunnie <dev.bhyunnie@gmail.com>
2025-04-11 19:17:25 +0900

    initial: 페이지네이션 프로젝트 생성

---
+++ be/kotlin/pagination/src/main/kotlin/example/bhyunnie/pagination/PaginationApplication.kt
@@ -0,0 +1,11 @@
+package example.bhyunnie.pagination
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaginationApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaginationApplication>(*args)
+}
---
+++ be/kotlin/pagination/src/test/kotlin/example/bhyunnie/pagination/PaginationApplicationTests.kt
@@ -0,0 +1,13 @@
+package example.bhyunnie.pagination
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaginationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
